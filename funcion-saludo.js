f9fd760bd1a0c0af858af839902b845390fa29d4
Angie Mnq <angiemanquillo5@gmail.com>
2026-04-29 14:40:52 -0500

    Fix console log syntax in saludarComunidad function

--- funcion-saludo.js
+++ funcion-saludo.js
@@ -1,7 +1,7 @@
 function saludarComunidad(nombreComunidad) {
     console.log("Bienvenida a todos:", nombreComunidad)
     console.log("Hoy seguimos aprendiendo JavaScript paso a paso.")
-    console.log ("Cada día la información nos enriquece como personas)
+    console.log ("Cada día la información nos enriquece como personas")
   }
   
   saludarComunidad("Programadores para la Paz e Igualdad de Colombia")
