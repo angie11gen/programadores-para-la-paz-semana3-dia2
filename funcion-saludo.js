2ad71cb75879d1a1e2054e942b6a47bdf8234763
Angie Mnq <angiemanquillo5@gmail.com>
2026-04-29 14:40:25 -0500

    Update greeting message and community name

--- funcion-saludo.js
+++ funcion-saludo.js
@@ -1,7 +1,8 @@
 function saludarComunidad(nombreComunidad) {
-    console.log("Bienvenida comunidad:", nombreComunidad)
+    console.log("Bienvenida a todos:", nombreComunidad)
     console.log("Hoy seguimos aprendiendo JavaScript paso a paso.")
+    console.log ("Cada día la información nos enriquece como personas)
   }
   
-  saludarComunidad("Programadores para la Paz")
+  saludarComunidad("Programadores para la Paz e Igualdad de Colombia")
   
